384926c9d615cb77507f7b794d00e56db5726ec0
Parthiba-Mukhopadhyay <meparthiba@gmail.com>
2023-06-25 10:17:58 +0530

    dice_change

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var val1=Math.floor(Math.random()*6)+1;
-var image1="images/"+"dice"+val+".png";
-document.querySelectorAll(".dice")[0].setAttribute(src,image1);
+const btn = document.querySelector(".butt");
 
-var val2=Math.floor(Math.random()*6)+1;
-var image2="images/"+"dice"+val+".png";
-document.querySelectorAll(".dice")[1].setAttribute(src,image2);
+btn.addEventListener("click", function () {
 
+  var val1 = Math.floor(Math.random() * 6) + 1;
+  var image1 = "images/" + "dice" + val1 + ".png";
+  document.querySelectorAll(".dice")[0].setAttribute("src", image1);
 
+  var val2 = Math.floor(Math.random() * 6) + 1;
+  var image2 = "images/" + "dice" + val2 + ".png";
+  document.querySelectorAll(".dice")[1].setAttribute("src", image2);
 
+});
