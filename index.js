6e5a5a9a7e5bce648efb7bb334b2fa969719b8c8
Parthiba-Mukhopadhyay <meparthiba@gmail.com>
2023-06-25 11:02:12 +0530

    final

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+
+var player1=prompt("Enter name of player1");
+var player2=prompt("Enter name of player2");
+document.querySelector(".p1").innerHTML=player1;
+document.querySelector(".p2").innerHTML=player2;
+
+
 const btn = document.querySelector(".butt");
 
 btn.addEventListener("click", function () {
@@ -9,5 +16,14 @@ btn.addEventListener("click", function () {
   var val2 = Math.floor(Math.random() * 6) + 1;
   var image2 = "images/" + "dice" + val2 + ".png";
   document.querySelectorAll(".dice")[1].setAttribute("src", image2);
+  
+  if(val1==val2)
+    document.querySelector("h1").innerHTML="&#11088; It's a Draw &#11088;";
+  
+  else if(val1>val2)
+    document.querySelector("h1").innerHTML="&#127942; "+player1+" wins";
+
+  else if(val1<val2)
+    document.querySelector("h1").innerHTML=player2+" wins &#127942;";
 
 });
