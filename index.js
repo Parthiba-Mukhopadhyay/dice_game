5a7c1a3d0063a5ba23458af90f49a5d1b5cd84d2
Parthiba-Mukhopadhyay <meparthiba@gmail.com>
2023-06-25 10:06:10 +0530

    draft1

---
+++ index.js
@@ -0,0 +1,10 @@
+var val1=Math.floor(Math.random()*6)+1;
+var image1="images/"+"dice"+val+".png";
+document.querySelectorAll(".dice")[0].setAttribute(src,image1);
+
+var val2=Math.floor(Math.random()*6)+1;
+var image2="images/"+"dice"+val+".png";
+document.querySelectorAll(".dice")[1].setAttribute(src,image2);
+
+
+
